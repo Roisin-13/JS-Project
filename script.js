e68ff57da4373d193e7bf00994400857bd078508
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-18 15:45:23 +0100

    form added to add employee - can only see in console

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+//--get listener for show
 document.getElementById("getEmployeeDataBtn").addEventListener("click", fetchEmployeeData);
+//--get listener for add
+document.getElementById("addEmployeeForm").addEventListener("submit", addEmployee);
 
+
+
+//======================show employee================//
 function fetchEmployeeData() {
     fetch("https://jsonplaceholder.typicode.com/users/")
         .then(response => response.json())
@@ -18,4 +24,25 @@ function fetchEmployeeData() {
 
             })
         })
+}
+//==========add employee===============//
+function addEmployee(e) {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let user = document.getElementById("userName").value;
+    let company = document.getElementById("company").value;
+    const myPost = {
+        name: name,
+        user: user,
+        company: company
+    }
+    fetch("https://jsonplaceholder.typicode.com/users/", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(myPost)
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
 }
